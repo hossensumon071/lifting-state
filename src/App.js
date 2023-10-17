c16b7ec2212fdf534198e17dd630ae653a3b4062
sumon hossen <hossensumon071@gmail.com>
2023-10-17 14:06:57 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-// import Inputs from "./components/Inputs";
+import React, { useState } from 'react'
+import ContactForm from './components/ContactForm'
+import Table from './components/Table'
 
-import SignUp from "./components/SignUp";
+const App = () => {
+  const [contacts, setContacts] = useState([])
 
-// import ControlledForm from "./components/ControlledForm";
-// import DataBinding from "./components/DataBinding";
-// import UncontrolledForm from "./components/UncontrolledForm";
+  const getContact = (values) => {
+    // console.log(values)
+    // console.log("calling data")
+    // console.log(values.name);
+    // console.log(values.email);
 
-function App() {
+    setContacts([...contacts, values])
+  }
+
   return (
     <div>
-      {/* <Inputs /> */}
-      {/* <UncontrolledForm/> */}
-      {/* <ControlledForm/> */}
-      {/* <DataBinding/> */}
-      <SignUp/>
+      <h1>Contact Form</h1>
+      <ContactForm getContact={getContact}/>
+      <Table contacts={contacts}/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
